112ff94b3c60262f1e6b96583823e753c6857e19
torerik123 <62653782+torerik123@users.noreply.github.com>
2024-09-05 15:53:12 +0200

    FEAT: Supabase setup

--- src/components/AppLibrary.vue
+++ src/components/AppLibrary.vue
@@ -63,10 +63,19 @@
 import { ref, type Ref, onMounted } from "vue"
 import LoginOrSignupBtn from "../components/LoginOrSignupBtn.vue"
 import ExtPay from "../../Extpay.js"
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://pysnzshgeafotwtersgp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5c256c2hnZWFmb3R3dGVyc2dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUzNjIyMDAsImV4cCI6MjA0MDkzODIwMH0.admFCKfM7V3kdwPbnA_3pONoX9hs_eRvuQubrYOfN0Q')
+
+// TODO
+// Supabase types 
 
 const extpay = ExtPay('samplewizard')
 
 const highlightColor = ref("#e255a1")
+
+const  isLoggedIn = ref<boolean>(false)
 
 //TODO: TS type file
 const savedFiles = ref<string[]>([
@@ -83,14 +92,46 @@ const savedFiles = ref<string[]>([
 	"file",
 ])
 
-const  isLoggedIn = ref<boolean>(false)
+const fetchUserFiles = async (userEmail: string) => {
+
+	// TODO
+		// supabase => Get files => pass in email
+		// in supabase check JWT => lookup email => get uuid
+		// get all files matching uuid 
+
+	const { data, error } = await supabase
+		.from('files')
+		.select('*')
+		.eq('uploaded_by', "e3cca84f-f0f8-4f99-b690-2c2f6aa29ef7");
+
+
+	// const { data, error } = await supabase
+	// 	.from('files')
+	// 	.select('*')
+	// 	.eq('email', userEmail);
+
+	if (error) {
+		console.error('Error fetching files:', error);
+		return [];
+	}
+
+	return data;
+};
+
 
 onMounted( async () : Promise<void> => {
 	const authUser = await extpay.getUser()
 
 	if (authUser?.paid) {	
 		isLoggedIn.value = true
+
+		console.log(authUser)
+
+		const files = fetchUserFiles(authUser.email)
+		console.log("files -- ", files)
 	}
+
+	
 })
 
 const playFile = (file) => {
